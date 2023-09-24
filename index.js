188e781e626f547a728938414d3417b1ca22ee49
abtirish <abtirish@gmail.com>
2023-09-23 18:56:46 -0600

    completes phase 0 pac 3 Function Paramters Lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+}
+
+const name = "Aki";
+const language = "Ember.js"
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
